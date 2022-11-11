15709829e003ec84e9c7c20d1834ee1de482a14d
ElsaApp360 <programadorelsa360@gmail.com>
2022-11-11 17:27:07 -0500

    Cambio de menu

--- JS/User/index.js
+++ JS/User/index.js
@@ -1,36 +1,38 @@
-let auth = null
-createAuth0Client({
-    domain: 'elsa360.us.auth0.com',
-    client_id: 'rYJuHduwZNTWCu6FTd2ZahaJx61grXWf'
-  }).then(auth0 => {
-    auth = auth0
-  });
-
-
-function loginUsuario() {
-    try {
-        debugger
-        auth.loginWithRedirect({
-            redirect_uri: window.location.origin + '/html/vertical-menu-template/dashboard.html'
-         }).then(token => {
-            //logged in. you can get the user profile like this:
-            auth.getUser().then(user => {
-              console.log(user);
-            });
-        })
-
-    } catch (e) {
-        console.log(e, 'Funcion -> Login Usuario');
-    }
-
-}
-
-function logout(){
-    try{
-        auth.logout({
-            returnTo: window.location.origin
-        });
-    }catch (e) {
-        console.log(e, 'Funcion -> Logout')
-    }
-}
+// let auth = null
+// createAuth0Client({
+//     domain: 'elsa360.us.auth0.com',
+//     client_id: 'rYJuHduwZNTWCu6FTd2ZahaJx61grXWf'
+//   }).then(auth0 => {
+//     auth = auth0
+//   });
+//
+//
+// function loginUsuario() {
+//
+//     //
+//     // try {
+//     //     debugger
+//     //     auth.loginWithRedirect({
+//     //         redirect_uri: window.location.origin + '/html/vertical-menu-template/dashboard.html'
+//     //      }).then(token => {
+//     //         //logged in. you can get the user profile like this:
+//     //         auth.getUser().then(user => {
+//     //           console.log(user);
+//     //         });
+//     //     })
+//     //
+//     // } catch (e) {
+//     //     console.log(e, 'Funcion -> Login Usuario');
+//     // }
+//
+// }
+//
+// function logout(){
+//     try{
+//         auth.logout({
+//             returnTo: window.location.origin
+//         });
+//     }catch (e) {
+//         console.log(e, 'Funcion -> Logout')
+//     }
+// }
