d060245cd7f162b2c5bc99ad9df2e1e584ca8110
Leonardo Barona <lebab1990@gmail.com>
2022-11-18 15:08:24 -0500

    Captura de los datos de listado general, manipulacion de los datos hasta obtener el par 'CLAVE:VALOR'

--- JS/dbData.js
+++ JS/dbData.js
@@ -1,14 +1,38 @@
 async function listar() {
     // console.log(apiServer)
-    let tabla = "perfilUsuario";
+    let tabla = "alimentoRegular";
     let campo = "*";
     let filtro = "1=1";
     let url = "https://" + apiServer + ":7155/CRUD/listar?tabla=" + tabla + "&filtro=" + filtro + "&campos=" + campo + "";
     await fetch(url)
         .then(response => response.json())
         .then(resultado => {
-            resultado.forEach(datos => {
-                console.log(datos);
+            datos = [];
+            resultado.forEach(element => {
+                dato = JSON.stringify(element);
+                datos.push(dato.split('"'));
             });
-        })
+            campo = [];
+            datos.forEach(element => {
+                element.forEach(subElement => {
+                    if ((subElement !== "[") && (subElement !== "]" && (subElement !== ","))) {
+                        campo.push(subElement);
+                    }
+                })
+            });
+            llave = [];
+            campo.forEach(element => {
+                llave.push(element.split(':'));
+            });
+            claves = [];
+            valor = [];
+            llave.forEach(element => {
+                claves.push(element[0]);
+                valor.push(element[1]);
+            });
+            for(let i=0;i<(claves.length);i++){
+                console.log(claves[i]+":"+valor[i]); 
+            }
+        });
+
 }
