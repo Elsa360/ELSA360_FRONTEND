ba3a2979433a1fd61db29cfd024bb03f325731a1
Erlin Briceño <erlinbrice@gmail.com>
2022-12-05 14:49:52 -0500

    Enviroment file modified to select o point to the correct DATABASE

--- JS/enviroment.js
+++ JS/enviroment.js
@@ -1,17 +1,18 @@
 console.log("SERVER: "+window.location.hostname);
 var dataBaseServer = "";
 var apiServer="";
-switch (window.location.hostname) { 
+switch (window.location.hostname) {
     case "127.0.0.1":
     case "localhost":
         apiServer="https://localhost:7155/";
+
+
         break;
     case "elsa360.com":
-        apiServer="";
+        apiServer="https://54.193.8.32/";
         break;
 
 
     default:
         break;
 }
-
