b1eaacab35c347d8a176251a9b4442fe3acfbd30
ElsaApp360 <programadorelsa360@gmail.com>
2022-12-17 00:33:31 -0500

    Entreno desde JS

--- JS/middleware.js
+++ JS/middleware.js
@@ -1,9 +1,9 @@
 function validateLogin(){
-    if(sessionStorage.getItem('login')){
-      //window.location.href = "dashboard.html"
-      console.log("usuario logueado");
-    }else{
-        window.location.href = "auth-login-basic.html"
-    }
+    // if(sessionStorage.getItem('login')){
+    //   //window.location.href = "dashboard.html"
+    //   console.log("usuario logueado");
+    // }else{
+    //     window.location.href = "auth-login-basic.html"
+    // }
 }
 validateLogin();
