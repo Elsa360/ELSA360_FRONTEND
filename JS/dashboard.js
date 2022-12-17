b79a8f8ade1748450ce7eb67225d9cf688d65e44
Erlin Briceño <erlinbrice@gmail.com>
2022-12-17 11:54:29 -0500

    pase a produccion detalle

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -1,7 +1,7 @@
 window.onload = async () => {
 
     let membresia = sessionStorage.getItem('membresia')
-    if (membresia == true) {
+    if (membresia == "ACTIVA") {
         let perfil = sessionStorage.getItem('perfil')
         fechaObjetivos(perfil)
         disponibilidad(perfil)
@@ -385,7 +385,7 @@ function graficosHorasDiarias(dataVolumnSemanal) {
             VolumenSemanal.render();
 
         }
-        //FIN  Grafico de Duración, Distancia y Elevanción de cada entreno 
+        //FIN  Grafico de Duración, Distancia y Elevanción de cada entreno
     } catch (e) {
         console.log("Graficos Disponibilidad Diaria: ", e)
     }
@@ -490,7 +490,7 @@ function diferenciaPeso(userWeightCurrent, userWeightWanted) {
 }
 function calcularEdad(fechaNacimiento) {
     try {
-        //Datos Fecha Actual    
+        //Datos Fecha Actual
         const fechaActual = new Date();
         const anoActual = parseInt(fechaActual.getFullYear());
         const mesActual = parseInt(fechaActual.getMonth()) + 1;
