7dba9006526d885d7fa31bd51cf7749223f36a6c
Erlin Briceño <erlinbrice@gmail.com>
2022-12-17 21:05:18 -0500

    inicio.html actualizado merge

--- JS/app-planDeportivoResumen.js
+++ JS/app-planDeportivoResumen.js
@@ -1,9 +1,8 @@
 
-document.getElementById("HabilidadesATrabajar").innerText="Fuerza y Resistencia";
-document.getElementById("TiempoTotalATrabajar").innerText="7 hrs";
+document.getElementById("Habilidad1ATrabajar").innerText="Fuerza";
+document.getElementById("Habilidad2ATrabajar").innerText="Resistencia";
+document.getElementById("TiempoTotalATrabajar").innerText="10 hrs";
 document.getElementById("FechaDeTrabajo").innerText="18/12/2022";
-var icono = document.getElementById('rm');
-        logo.attr('src','img/rm2.png');
 
 
 
@@ -23,7 +22,6 @@ var icono = document.getElementById('rm');
 var parrafo = document.createElement("p");
 var flecha = document.createElement("i");
 var nombreEjercicio = document.createElement("span");
-var espacio = document.createElement("br");
 var realiza = document.createElement("span");
 var series = document.createElement("span");
 var Serie = document.createElement("span");
@@ -64,36 +62,36 @@ var entreSeries = document.createElement("span")
 
 
 // CONTENIDO DE LOS CONTENEDORES
-var contenidoNombreEjercicio = document.createTextNode("Haz fuerza sobre los pedales, con las manos cogidas de la parte inferior del manubrio.");
-var RealizaContenido = document.createTextNode ("Realiza ");
+var contenidoNombreEjercicio = document.createTextNode("");
+var RealizaContenido = document.createTextNode ("");
 
 var contenidoSeries = document.createTextNode (" 1");
 
 var SerieContenido = document.createTextNode (" serie(s)");
-var deContenido = document.createTextNode (" de");
+var deContenido = document.createTextNode (" X");
 
 var contenidoRepeticiones = document.createTextNode (" 5");
 
-var contenidoRepeticion = document.createTextNode (" repeticiones");
-var de2Contenido = document.createTextNode (" de");
+var contenidoRepeticion = document.createTextNode (" Reps");
+var de2Contenido = document.createTextNode (" X");
 var tiempoRepeticionContenido = document.createTextNode (" 20");
-var MagnitudTiempoRepeticionContenido = document.createTextNode (" minutos");
-var cadaUnaContenido = document.createTextNode (" cada una, al ");
+var MagnitudTiempoRepeticionContenido = document.createTextNode (" min");
+var cadaUnaContenido = document.createTextNode (" X");
 var PorcentajeIntensidadContenido = document.createTextNode (" 80% ");
-var intensidadContenido = document.createTextNode (" de intensidad ");
-var manejandoContenido = document.createTextNode (" y manejando una cadencia entre ");
+var intensidadContenido = document.createTextNode (" Int ");
+var manejandoContenido = document.createTextNode (" X ");
 var cadenciaContenido = document.createTextNode (" 75 - 80 ");
 var MagnitudCadenciaContenido = document.createTextNode (" RPM ");
-var enContenido = document.createTextNode (" en ");
+var enContenido = document.createTextNode (" X ");
 var terrenoContenido = document.createTextNode (" terreno plano ");
-var yContenido = document.createTextNode (" y ");
+var yContenido = document.createTextNode (" X ");
 var posicionContenido = document.createTextNode (" parandote en pedales");
-var descansandoContenido = document.createTextNode ("Descansando ");
+var descansandoContenido = document.createTextNode ("Descansa ");
 var tiempoDescansoRepsContenido = document.createTextNode (" 10");
-var MagnitudTiempoDescansoRepsContenido = document.createTextNode (" minutos");
-var alContenido = document.createTextNode (" al "); 
+var MagnitudTiempoDescansoRepsContenido = document.createTextNode (" min");
+var alContenido = document.createTextNode (" X "); 
 var descansoIntensidadContenido = document.createTextNode (" 50% ");
-var MagnitudDescansoIntensidadContenido = document.createTextNode (" de intensidad ");
+var MagnitudDescansoIntensidadContenido = document.createTextNode (" Intensidad ");
 var entreRepsContenido = document.createTextNode (" entre repeticiones y ");
 var descansoSeriesContenido = document.createTextNode (" 5 ");
 var MagnitudDescansoSeriesContenido = document.createTextNode (" minutos ");
@@ -112,76 +110,76 @@ nombreEjercicio.style = "color: #cbcbe2; font-weight: 400";
 realiza.style = "color: #cbcbe2; font-weight: 400";
 
 series.className = "text-success";
-series.style = "font-size: 16px; font-weight: 600"; 
+series.style = "font-size: 15px; font-weight: 600"; 
 
 Serie.className ="text-success";
-Serie.style = "font-size: 16px; font-weight: 600";
+Serie.style = "font-size: 15px; font-weight: 600";
 
 de.style = "color: #cbcbe2; font-weight: 400";
 
 repeticiones.className = "text-success";
-repeticiones.style = "font-size: 16px; font-weight: 600"; 
+repeticiones.style = "font-size: 15px; font-weight: 600"; 
 
 repeticion.className ="text-success";
-repeticion.style = "font-size: 16px; font-weight: 600";
+repeticion.style = "font-size: 15px; font-weight: 600";
 
 de2.style = "color: #cbcbe2; font-weight: 400";
 
 tiempoRepeticion.className = "text-success";
-tiempoRepeticion.style = "font-size: 16px; font-weight: 600"; 
+tiempoRepeticion.style = "font-size: 15px; font-weight: 600"; 
 
 MagnitudTiempoRepeticion.className ="text-success";
-MagnitudTiempoRepeticion.style = "font-size: 16px; font-weight: 600";
+MagnitudTiempoRepeticion.style = "font-size: 15px; font-weight: 600";
 
 cadaUna.style = "color: #cbcbe2; font-weight: 400";
 
 PorcentajeIntensidad.className ="text-success";
-PorcentajeIntensidad.style = "font-size: 16px; font-weight: 600";
+PorcentajeIntensidad.style = "font-size: 15px; font-weight: 600";
 
 intensidad.className ="text-success";
-intensidad.style = "font-size: 16px; font-weight: 600"
+intensidad.style = "font-size: 15px; font-weight: 600"
 
 manejando.style = "color: #cbcbe2; font-weight: 400";
 
 cadencia.className = "text-success";
-cadencia.style = "font-size: 16px; font-weight: 600"; 
+cadencia.style = "font-size: 15px; font-weight: 600"; 
 
 MagnitudCadencia.className ="text-success";
-MagnitudCadencia.style = "font-size: 16px; font-weight: 600";
+MagnitudCadencia.style = "font-size: 15px; font-weight: 600";
 
 en.style = "color: #cbcbe2; font-weight: 400";
 
 terreno.className = "text-success";
-terreno.style = "font-size: 16px; font-weight: 600"; 
+terreno.style = "font-size: 15px; font-weight: 600"; 
 
 y.style = "color: #cbcbe2; font-weight: 400";
 
 posicion.className = "text-success";
-posicion.style = "font-size: 16px; font-weight: 600"; 
+posicion.style = "font-size: 15px; font-weight: 600"; 
 
 descansando.style = "color: #cbcbe2; font-weight: 400";
 
 tiempoDescansoReps.className = "text-success";
-tiempoDescansoReps.style = "font-size: 16px; font-weight: 600"; 
+tiempoDescansoReps.style = "font-size: 15px; font-weight: 600"; 
 
 MagnitudTiempoDescansoReps.className ="text-success";
-MagnitudTiempoDescansoReps.style = "font-size: 16px; font-weight: 600";
+MagnitudTiempoDescansoReps.style = "font-size: 15px; font-weight: 600";
 
 al.style = "color: #cbcbe2; font-weight: 400";
 
 descansoIntensidad.className = "text-success";
-descansoIntensidad.style = "font-size: 16px; font-weight: 600"; 
+descansoIntensidad.style = "font-size: 15px; font-weight: 600"; 
 
 MagnitudDescansoIntensidad.className ="text-success";
-MagnitudDescansoIntensidad.style = "font-size: 16px; font-weight: 600";
+MagnitudDescansoIntensidad.style = "font-size: 15px; font-weight: 600";
 
 entreReps.style = "color: #cbcbe2; font-weight: 400";
 
 descansoSeries.className = "text-success";
-descansoSeries.style = "font-size: 16px; font-weight: 600"; 
+descansoSeries.style = "font-size: 15px; font-weight: 600"; 
 
 MagnitudDescansoSeries.className ="text-success";
-MagnitudDescansoSeries.style = "font-size: 16px; font-weight: 600";
+MagnitudDescansoSeries.style = "font-size: 15px; font-weight: 600";
 
 entreSeries.style = "color: #cbcbe2; font-weight: 400";
 
@@ -246,7 +244,6 @@ entreSeries.appendChild(entreSeriesContenido);
 
 parrafo.appendChild(flecha);
 parrafo.appendChild(nombreEjercicio);
-parrafo.appendChild(espacio);
 
 parrafo.appendChild(realiza);
 parrafo.appendChild(series);
--- JS/dashboardGraficos.js
+++ JS/dashboardGraficos.js
@@ -158,3 +158,419 @@ Apex.chart= {
       
     }
       //FIN  Grafico de Duración, Distancia y Elevanción de cada entreno 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      // grafico de duraciòn, distancia y desnivel semanal
+dataDuracionAcumSem=[23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30, 23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30, 23, 11]
+dataDuracionPromSem=[23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30, 23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30, 23, 11]
+
+dataDesnivelAcumSem=[1044, 1255, 1141, 1067, 1222, 1343, 1021, 1241, 1456, 1027, 1243, 1144, 1455, 1041, 1267, 1022, 1243, 1221, 1041, 1056, 1227, 1143, 1244, 1055]
+dataDesnivelPromSem=[1044, 1255, 1141, 1067, 1222, 1343, 1021, 1241, 1456, 1027, 1243, 1144, 1455, 1041, 1267, 1022, 1243, 1221, 1041, 1056, 1227, 1143, 1244, 1055]
+
+dataDistanciaAcumSem=[68.30, 71.25, 92.36, 101.30, 79.45, 92.35, 101.64, 79.52, 92.59, 101.36, 79.39, 92.30, 101.25, 79.36, 92.30, 101.45, 79.35, 92.64, 101.52, 79.59, 92.36, 101.39, 79.30, 92.25]
+dataDistanciaPromSem=[68.30, 71.25, 92.36, 101.30, 79.45, 92.35, 101.64, 79.52, 92.59, 101.36, 79.39, 92.30, 101.25, 79.36, 92.30, 101.45, 79.35, 92.64, 101.52, 79.59, 92.36, 101.39, 79.30, 92.25]
+
+dataLabelsSem=['01/01/2022', '01/02/2022', '01/03/2022', '01/04/2022', '01/05/2022', '01/06/2022', '01/07/2022', '01/08/2022', '01/09/2022', '01/10/2022', '01/11/2022', '01/12/2022', '01/13/2022', '01/14/2022', '01/15/2022', '01/16/2022', '01/17/2022', '01/18/2022', '01/19/2022', '01/20/2022', '01/21/2022', '01/22/2022', '01/23/2022', '01/24/2022']
+
+durElvDistSemana(
+  dataDuracionAcumSem, dataDuracionPromSem, dataDesnivelAcumSem, dataDesnivelPromSem, dataLabelsSem
+  );
+
+function durElvDistSemana(
+  dataDuracionAcumSem, dataDuracionPromSem, dataDesnivelAcumSem, dataDesnivelPromSem, dataLabelsSem
+  ) {
+
+
+var optionsDistAcumYPromSem1 = {
+  colors: ["#9bcb3b","#9bcb3b", "#ffab00","#ffab00","#007bff","#007bff"],
+    series: [
+      {
+      name: 'Duración Acumulada',
+      type: 'bar',
+      data: dataDuracionAcumSem
+    }, {
+      name: 'Duración Promedio',
+      type: 'line',
+      data: dataDuracionPromSem
+    }, {
+      name: 'Desnivel Positivo Acumulado',
+      type: 'bar',
+      data: dataDesnivelAcumSem
+    },
+    {
+      name: 'Desnivel Positivo Promedio',
+      type: 'line',
+      data: dataDesnivelPromSem
+    }, {
+      name: 'Distancia Acumulada',
+      type: 'bar',
+      data: dataDistanciaAcumSem
+    }, {
+      name: 'Distancia Promedio',
+      type: 'line',
+      data: dataDistanciaPromSem
+    },
+    ],
+  chart: {
+    height: 350,
+    background: '#2b2c40',
+    type: 'line',
+    zoom:{
+      enabled: true,
+      type: 'x',
+      autoScaleYaxis: true
+    },
+    toolbar: {
+      show: true,
+      autoSelected:'zoom',
+      offsetX:-20,
+      offsetY:-20,
+      tools: {
+        download: true,
+        selection: true,
+        zoom: true,
+        zoomin: true,
+        zoomout: true,
+        pan: true,
+        reset: true | '<img src="/static/icons/reset.png" width="20">',
+        customIcons: []
+      },
+      export:{
+        csv:{
+          filename: 'Datos básicos semanales'
+        },
+        svg:{
+          filename: 'Datos básicos semanales'
+        },
+        png:{
+          filename: 'Datos básicos semanales'
+        }
+      }
+    }
+  },
+  title: {
+    text: 'Datos básicos semanales',
+    offsetX: 10,
+    style: {
+      fontSize:  '16px',
+      fontWeight:  600,
+      fontFamily:  'montserrat',
+      color:  '#cbcbe2'
+    },
+  },
+  tooltip: {
+    fixed: {
+      enabled: true,
+      shared: true,
+      position: 'topLeft', // topRight, topLeft, bottomRight, bottomLeft
+      offsetY: 30,
+      offsetX: 60
+    },
+    y: [
+      {
+          formatter: function (val) {
+            return val + " h:m:s"
+          }
+      },
+      {
+          formatter: function (val) {
+            return val + " h:m:s"
+          }
+      },
+      {
+        formatter: function (val) {
+          return val + " m"
+        }
+    },
+    {
+      formatter: function (val) {
+        return val + " m"
+      }
+  },
+  {
+      formatter: function (val) {
+        return val + " Km"
+      }
+  },
+  {
+    formatter: function (val) {
+      return val + " Km"
+    }
+}
+    ]
+  },
+  plotOptions: {
+    bar: {
+      columnWidth: '70%',
+      borderRadius: 1
+    }
+  },
+  fill: {
+    opacity: [0.55, 0.75, 0.55, 0.75, 0.55, 0.75],
+    gradient: {
+      inverseColors: false,
+      shade: 'light',
+      type: "vertical",
+      opacityFrom: 0.1,
+      opacityTo: 0.01,
+      stops: [0, 100, 100, 100]
+    }
+  },
+  stroke: {
+    width: [2, 4, 2, 4, 2, 4],
+    curve: 'smooth',
+    colors: ["#9bcb3b","#9bcb3b", "#ffab00","#ffab00","#007bff","#007bff"],
+  },
+  dataLabels: {
+    enabled: false   
+  },
+  markers: {
+    size: 4,
+    colors: ["#9bcb3b","#9bcb3b", "#ffab00","#ffab00","#007bff","#007bff"],
+    strokeColors: ["#9bcb3b","#9bcb3b", "#ffab00","#ffab00","#007bff","#007bff"],
+  },
+  labels: dataLabelsSem,
+  xaxis: {
+    type: 'datetime',
+    labels: {
+      style: {
+        colors: ['#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2']
+      }
+    },
+    group: {
+      style: {
+        fontSize: '13px',
+        fontWeight: 400,
+        colors: ['#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2', '#cbcbe2',]
+      },
+      groups: [
+        { title: 'Macrociclo 1', cols: 3 },
+        { title: 'Macrociclo 2', cols: 3 },
+        { title: 'Macrociclo 3', cols: 3},
+        { title: 'Macrociclo 4', cols: 3}
+      ]
+    }
+  },
+  yaxis: [
+    {
+      axisTicks: {
+        show: true,
+        color: '#9bcb3b',
+        offsetX: 5,
+      },
+      axisBorder: {
+        show: true,
+        color: '#9bcb3b',
+        offsetX: -5,
+      },
+      labels: {
+        rotate: 90,
+        offsetX: -5,
+        style: {
+          colors: '#9bcb3b',
+          fontSize:'10px'
+        }
+      },       
+      tooltip: {
+        enabled: false
+      }
+    },
+    {
+      opposite: true,
+      axisTicks: {
+        show: true,
+        color: '#9bcb3b',
+        offsetX: 10,
+      },
+      axisBorder: {
+        show: true,
+        color: '#9bcb3b',
+        offsetX: -10,
+      },
+      labels: {
+        rotate: 90,
+        offsetX: -10,
+        style: {
+          colors: '#9bcb3b',
+          fontSize:'10px'
+        }
+      },       
+      tooltip: {
+        enabled: false
+      }
+    },
+    {
+      axisTicks: {                                    
+        show: true,
+        color: '#ffab00',
+        offsetX: 8,
+      },
+      axisBorder: {
+        show: true,
+        color: '#ffab00',
+        offsetX: -10,
+      },
+      labels: {
+        rotate: 90,
+        offsetX: 10,
+        style: {
+          colors: '#ffab00',
+          fontSize:'9px',
+        },
+      },       
+    },
+    {
+      opposite: true,
+      axisTicks: {
+        show: true,
+        color: '#ffab00',
+        offsetX: 3,
+      },
+      axisBorder: {
+        show: true,
+        color: '#ffab00',
+        offsetX: -3,
+      },
+      labels: {
+        rotate: 90,
+        offsetX: -3,
+        style: {
+          colors: '#ffab00',
+          fontSize:'9px',
+        },
+      },       
+    },
+    {
+      axisTicks: {
+        show: true,
+        color: '#007bff',
+        offsetX: 8,
+      },
+      axisBorder: {
+        show: true,
+        color: '#007bff',
+        offsetX: -8,
+      },
+      labels: {
+        rotate: 90,
+        offsetX: 12,
+        style: {
+          colors: '#007bff',
+          fontSize:'9px',
+        },
+      },       
+    },
+    {
+      opposite: true,
+      axisTicks: {
+        show: true,
+        color: '#007bff',
+        offsetX: 3,
+      },
+      axisBorder: {
+        show: true,
+        color: '#007bff',
+        offsetX: -3,
+      },
+      labels: {
+        rotate: 90,
+        offsetX: -3,
+        style: {
+          colors: '#007bff',
+          fontSize:'9px',
+        },
+      },       
+    },       
+  ],
+  legend: {
+    show: true,
+    offsetY: 5,
+    position: 'bottom',
+    horizontalAlign: 'center',
+    fontSize: '12px',
+    itemMargin: {
+      horizontal: 20,
+      vertical: 8
+  },
+    labels: {
+      colors: '#fff',
+      useSeriesColors: false
+    }
+  },
+  grid: {
+    show: true,
+    borderColor: '#191924',
+    strokeDashArray: 0,
+    position: 'back',
+    xaxis: {
+      lines: {
+        show: false
+      }
+    },
+    yaxis: {
+      lines: {
+        show: true
+      }
+    },
+    row: {
+      colors: undefined,
+      opacity: 0.1
+    },
+    column: {
+      colors: undefined,
+      opacity: 0.5
+    },
+    padding: {
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0
+    }
+  },
+  
+};
+
+var DistAcumYPromSem1 = new ApexCharts(document.querySelector("#DistAcumYPromSem1"), optionsDistAcumYPromSem1);
+DistAcumYPromSem1.render();
+DistAcumYPromSem1.toggleSeries('Duración Promedio')
+DistAcumYPromSem1.toggleSeries('Desnivel Positivo Acumulado')
+DistAcumYPromSem1.toggleSeries('Distancia Promedio')
+}
+//FIN grafico de duraciòn, distancia y desnivel semanal
