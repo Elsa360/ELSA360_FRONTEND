f391b49e77d3bfa534bb33a23399367463c96a60
Erlin Briceño <erlinbrice@gmail.com>
2022-12-20 22:18:54 -0500

    s

--- _avatar.php
+++ _avatar.php
@@ -87,7 +87,7 @@ if ($uploadOk == 0) {
   </script>
 
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
+<form action="_avatar.php" method="post" enctype="multipart/form-data">
   Select image to upload:
   <input type="file" name="fileToUpload" id="fileToUpload">
   <input type="submit" value="Upload Image" name="submit">
