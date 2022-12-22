59bb0f5b7cfb3d61758026f47e2f20be31fe6cf3
Erlin Briceño <erlinbrice@gmail.com>
2022-12-22 18:28:22 -0500

    actualización de la variable de sessión

--- JS/User/user.js
+++ JS/User/user.js
@@ -508,7 +508,8 @@ function guardarPerilUsuario(){
   .then((respuesta) => {
     console.log(respuesta);
     localStorage.getItem("perfil", respuesta);
-    location.href = "dashboard.html";
+    loginNoPass(localStorage.idUsuario);
+    //location.href = "dashboard.html";
     //console.log(respuesta);
   });
 }
